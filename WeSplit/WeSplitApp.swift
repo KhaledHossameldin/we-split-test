84ded3a3437c64a841c45eee74eda88c4af303d1
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-07-25 17:43:05 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Khaled Hossameldin on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
